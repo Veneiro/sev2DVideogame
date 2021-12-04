2d80372955a337508b45b41748438fca23505937
Veneiro <veneiroyt@gmail.com>
2021-12-04 19:19:05 +0100

    Creating PowerUPS

---
+++ NavesL4/PU_Ammo.cpp
@@ -0,0 +1,6 @@
+#include "PU_Ammo.h"
+
+PU_Ammo::PU_Ammo(float x, float y, Game* game)
+	: Actor("res/corazon.png", x, y, 44, 36, game) {
+
+}
---
+++ NavesL4/PU_Ammo.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Actor.h"
+class PU_Ammo :
+	public Actor
+{
+public:
+	PU_Ammo(float x, float y, Game* game);
+};
+
